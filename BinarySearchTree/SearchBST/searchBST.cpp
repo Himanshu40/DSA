9d426a0274da81ff5f9f0218c062dab2c6ba0e91
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2021-12-20 17:52:54 +0530

    Added Binary Search Tree

---
+++ BinarySearchTree/SearchBST/searchBST.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode() {
+        if (left) delete left;
+        if (right) delete right;
+    }
+};
+
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+bool searchInBST(BinaryTreeNode<int> *root , int k) {
+    if (!root) {
+        return false;
+    }
+    
+    if (root->data == k) {
+        return true;
+    }
+
+    bool check;
+
+    if (root->data > k) {
+        check = searchInBST(root->left, k);
+    }
+    else {
+        check = searchInBST(root->right, k);
+    }
+
+    return check;
+}
+
+int main() {
+    BinaryTreeNode<int> *root = takeInput();
+    int k;
+    cin >> k;
+    cout << ((searchInBST(root, k)) ? "true" : "false");
+    delete root;
+}
